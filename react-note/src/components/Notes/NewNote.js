e9eac2a6e3f45f8252487e5d856f79a22a47eb93
Eva <evalee904@gmail.com>
2019-01-05 19:51:10 -0600

    Refactored new note component and its view

--- react-note/src/components/Notes/NewNote.js
+++ react-note/src/components/Notes/NewNote.js
@@ -1,108 +1,33 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import styled from 'styled-components';
+import React from 'react';
 
-export default class NewNote extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: '',
-			content: ''
-		};
-	}
+import { FormContainer, FormGroup, ActionButton } from '../../styles';
 
-	addNewNote = event => {
-		event.preventDefault();
-		axios
-			.post('https://agile-bastion-89851.herokuapp.com/api/notes', this.state)
-			.then(() => this.props.history.push('/'))
-			.catch(err => {
-				console.error(err);
-			});
-	};
+const NewNote = props => {
+	return (
+		<FormContainer>
+			<h2>Create New Note:</h2>
 
-	handleInput = event => {
-		this.setState({ [event.target.name]: event.target.value });
-	};
+			<FormGroup onSubmit={props.addNote}>
+				<input
+					type="text"
+					placeholder="Note Title"
+					onChange={props.handleInput}
+					name="title"
+					value={props.title}
+				/>
 
-	render() {
-		return (
-			<NewNoteDiv>
-				<h3>Create New Note:</h3>
-				<form onSubmit={this.addNewNote}>
-					<input
-						className="note-title"
-						type="text"
-						onChange={this.handleInput}
-						placeholder=" Note Title"
-						value={this.state.title}
-						name="title"
-					/>
-					<textarea
-						rows="20"
-						className="note-text"
-						type="text"
-						onChange={this.handleInput}
-						placeholder=" Note Content"
-						value={this.state.content}
-						name="content"
-					/>
-					<button type="submit">Save</button>
-				</form>
-			</NewNoteDiv>
-		);
-	}
-}
+				<textarea
+					type="text"
+					placeholder="Note Content"
+					onChange={props.handleInput}
+					name="textBody"
+					value={props.textBody}
+				/>
 
-const NewNoteDiv = styled.div`
-	box-sizing: border-box;
-	border: 1px solid #bebebe;
-	background-color: #f3f3f3;
-	min-width: 1000px;
-	min-height: 1000px;
-	padding: 0 30px;
+				<ActionButton type="submit">Save</ActionButton>
+			</FormGroup>
+		</FormContainer>
+	);
+};
 
-	h3 {
-		color: #4a494a;
-		margin-top: 50px;
-	}
-
-	form {
-		box-sizing: border-box;
-		display: flex;
-		flex-direction: column;
-
-		.note-title {
-			border: 1px solid #bebebe;
-			border-radius: 3px;
-			width: 380px;
-			height: 40px;
-			margin: 15px 0;
-			font-size: 16px;
-		}
-
-		.note-text {
-			border: 1px solid #bebebe;
-			border-radius: 3px;
-			width: 650px;
-			font-size: 16px;
-			margin: 15px 0;
-		}
-
-		button {
-			box-sizing: border-box;
-			height: 50px;
-			width: 220px;
-			background-color: #2ac0c4;
-			color: #ffffff;
-			text-decoration: none;
-			font-weight: bold;
-			margin: 15px 0;
-			text-align: center;
-			line-height: 50px;
-			border: 1px solid #bebebe;
-			font-size: 16px;
-			cursor: pointer;
-		}
-	}
-`;
+export default NewNote;
