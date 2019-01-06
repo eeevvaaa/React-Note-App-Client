a65bb3bc919e8b7e57f07951caacad66de886fab
Eva <evalee904@gmail.com>
2019-01-05 19:52:50 -0600

    Added index file for view folder

---
+++ react-note/src/views/index.js
@@ -0,0 +1,4 @@
+export { default as DetailView } from './DetailView';
+export { default as EditView } from './EditView';
+export { default as ListView } from './ListView';
+export { default as NewView } from './NewView';
