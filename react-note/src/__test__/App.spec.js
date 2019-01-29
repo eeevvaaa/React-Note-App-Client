57b3e22ccaab26ec62361ef26c5bdfb813924562
Eva <evalee904@gmail.com>
2019-01-29 00:21:14 -0600

    Added snapshot matcher

--- react-note/src/__test__/App.spec.js
+++ react-note/src/__test__/App.spec.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import App from '../App';
 
@@ -9,5 +12,10 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('<App />', () => {
 	it('renders without crashing', () => {
 		shallow(<App />);
+	});
+
+	it('should match snapshot', () => {
+		const snapshot = renderer.create(withRouter(connect(<App />))).toJSON();
+		expect(snapshot).toMatchSnapshot();
 	});
 });
